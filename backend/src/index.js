1c4c87905a7241134a3decfe7f79af1d7099fab7
balaji-saw <balajisawdts@gmail.com>
2025-12-07 01:08:59 +0530

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
     origin: [
         'http://localhost:5173',
-        'http://localhost:5174',
+        'https://truestate-sdia.vercel.app',
         'https://truestatefrontend.vercel.app'
     ],
     credentials: true
